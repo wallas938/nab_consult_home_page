87f7dfc9959ea3bc5492d3a8b302138d04f1dfce
wallas938 <drame.sissako001@gmail.com>
2024-08-21 16:15:12 +0200

    la section partenaires est operationnelles sur les trois ecran

--- lib/main.dart
+++ lib/main.dart
@@ -32,78 +32,72 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: const Size(320, 497),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (context, child) {
-          return MaterialApp(
-            home: Scaffold(
-              backgroundColor: Colors.white,
-              appBar: PreferredSize(
-                preferredSize: !ResponsiveUtil.isOnDesktop(context) ? const Size.fromHeight(50) : const Size.fromHeight(80),
-                child: AppBar(
-                  flexibleSpace: Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: ResponsiveUtil.isOnDesktop(context)
-                          ? [
-                              TextButton(
-                                  onPressed: () {
-                                    _updatedIndex(0);
-                                  },
-                                  child: Text("Accueil",
-                                      style: styleCurrentLink(0))),
-                              TextButton(
-                                  onPressed: () {
-                                    _updatedIndex(1);
-                                  },
-                                  child: Text("Qui sommes nous ?",
-                                      style: styleCurrentLink(1))),
-                              TextButton(
-                                  onPressed: () {
-                                    _updatedIndex(2);
-                                  },
-                                  child: Text("Nous contacter",
-                                      style: styleCurrentLink(2))),
-                            ]
-                          : [],
-                    ),
-                  ),
-                  centerTitle: false,
-                  title: ResponsiveUtil.isOnDesktop(context)
-                      ? const Text("NAB CONSULT")
-                      : null,
-                  leading: !ResponsiveUtil.isOnDesktop(context)
-                      ? Builder(
-                          builder: (context) {
-                            return IconButton(
-                              icon: const Icon(Icons.menu),
-                              onPressed: () {
-                                Scaffold.of(context).openDrawer();
-                              },
-                            );
-                          },
-                        )
-                      : null,
-                ),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: PreferredSize(
+          preferredSize: !ResponsiveUtil.isOnDesktop(context)
+              ? const Size.fromHeight(50)
+              : const Size.fromHeight(80),
+          child: AppBar(
+            flexibleSpace: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: ResponsiveUtil.isOnDesktop(context)
+                    ? [
+                        TextButton(
+                            onPressed: () {
+                              _updatedIndex(0);
+                            },
+                            child: Text("Accueil", style: styleCurrentLink(0))),
+                        TextButton(
+                            onPressed: () {
+                              _updatedIndex(1);
+                            },
+                            child: Text("Qui sommes nous ?",
+                                style: styleCurrentLink(1))),
+                        TextButton(
+                            onPressed: () {
+                              _updatedIndex(2);
+                            },
+                            child: Text("Nous contacter",
+                                style: styleCurrentLink(2))),
+                      ]
+                    : [],
               ),
-              drawer:
-                  !ResponsiveUtil.isOnDesktop(context) ? const MyDrawer() : null,
-              body: Container(
-                  decoration: const BoxDecoration(
-                      gradient: LinearGradient(colors: [
-                    Color.fromRGBO(234, 234, 234, 1),
-                    Color.fromRGBO(219, 219, 219, 1),
-                    Color.fromRGBO(242, 242, 242, 1),
-                    Color.fromRGBO(173, 169, 150, 1)
-                  ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-                  child: const MyHomePage()),
             ),
-            debugShowCheckedModeBanner: false,
-          );
-        });
+            centerTitle: false,
+            title: ResponsiveUtil.isOnDesktop(context)
+                ? const Text("NAB CONSULT")
+                : null,
+            leading: !ResponsiveUtil.isOnDesktop(context)
+                ? Builder(
+                    builder: (context) {
+                      return IconButton(
+                        icon: const Icon(Icons.menu),
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                      );
+                    },
+                  )
+                : null,
+          ),
+        ),
+        drawer: !ResponsiveUtil.isOnDesktop(context) ? const MyDrawer() : null,
+        body: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Color.fromRGBO(234, 234, 234, 1),
+              Color.fromRGBO(219, 219, 219, 1),
+              Color.fromRGBO(242, 242, 242, 1),
+              Color.fromRGBO(173, 169, 150, 1)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+            child: const MyHomePage()),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
